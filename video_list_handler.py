7f4b08ccc9c9103f700806909a9d25cd5c058824
Cristian_Gh <cristocea_89@yahoo.com>
2024-01-02 10:21:25 +0200

    add subtitle to list of videos

--- video_list_handler.py
+++ video_list_handler.py
@@ -76,3 +76,35 @@ def add_video_to_list(file_name, video_name, duration, subtitle_name, subtitle_l
         })
     with open(file_name, 'w') as f:
         json.dump(data, f, indent=4)
+
+
+def add_subtitle_to_list(file_name, video_name, duration, subtitle_name, subtitle_language):
+    print(f"Adding subtitle {subtitle_name} to video {video_name} in video_list.json")
+    with open(file_name, 'r') as f:
+        # load json file and replace ip of tracks and hls for "Movies" category
+        data = json.load(f)
+        video_found = False
+        for video in data["categories"][0]["videos"]:
+            if video["title"] == video_name:
+                # get tracks size and add new track with different id
+                track_size = len(video["tracks"])
+                video["tracks"].append({
+                    "id": str(track_size + 1),
+                    "type": "text",
+                    "subtype": "captions",
+                    "contentId": video_name + ".vtt",
+                    "name": subtitle_name,
+                    "language": subtitle_language
+                })
+                video_found = True
+                break
+        if not video_found:
+            print(f"Video {video_name} not found in video_list.json")
+            add_video_to_list(file_name, video_name, 0, subtitle_name, subtitle_language)
+
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+if __name__ == '__main__':
+    add_video_to_list('video_list.json', "apvral-scenes.from.a.marriage.720p[flt]", 10196, "English", "en")
